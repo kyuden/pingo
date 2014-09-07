645bfd3d52899a1f3a21ff58b17a2f777c0f35f6
Kyuden <msmsms.um@gmail.com>
2014-09-07 20:11:28 +0900

    v0.0.1

---
+++ lib/pingo/version.rb
@@ -0,0 +1,3 @@
+module Pingo
+  VERSION = "0.0.1.1"
+end
---
+++ spec/pingo_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Pingo do
+  it 'has a version number' do
+    expect(Pingo::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
